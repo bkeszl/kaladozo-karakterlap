73dcb168ca60d62efd39019c6bcf7432c8dfeada
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-09-14 11:57:45 +0200

    add sequelize connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,23 @@
 const express = require('express')
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_URL, process.env.DB_USER, process.env.DB_PASS, {
+  host: "127.0.0.1",
+  dialect: "mysql"
+})
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testConnection();
+
 const app = express()
 const port = 3000
 
